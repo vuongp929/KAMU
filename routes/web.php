1daf0cdf413ae3f395105a35ab01db4a478b6f03
TranNgocKien02 <kientnph41341@fpt.edu.vn>
2025-06-22 22:31:27 +0700

    quan ly don hang

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,8 +16,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+      Route::resource('orders', OrderController::class);
 });
+
+// Group route admin với middleware bảo vệ
+Route::middleware(['auth'])->prefix('admin')->group(function () {
+    Route::resource('/orders', OrderController::class);
+});
+
 Route::get('/test-cache-driver', function () {
     return config('cache.default');
 });
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
